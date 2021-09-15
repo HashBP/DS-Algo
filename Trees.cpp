6d32b4e61a83296ec66b0da91fe13d93a625861a
Harsh <Hvp6848@gmail.com>
2021-09-15 16:00:09 +0530

    First commit for Trees

---
+++ Trees.cpp
@@ -0,0 +1,7 @@
+//Trees - Collection of nodes and data connected with each other in singular form.
+//Collection of Trees is called Forest.
+
+//Binary Tree - The number of nodes can not be excided more then two.
+//Number of Binary Trees without Identity = (2n C n)/(n+1)
+//Number of Binary Trees with Identity = (2n C n)*n!/(n+1)
+//Number of Nodes with Maximum Height = 2^(n-1)
